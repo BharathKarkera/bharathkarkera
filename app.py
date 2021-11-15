f36f751ed93a1d19097b963613c8b8aff617bcb1
BharathKarkera <94306701+BharathKarkera@users.noreply.github.com>
2021-11-15 13:26:51 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+import flask
+
+app = flask.Flask(__name__)
+app.config['DEBUG'] = True
+app.config['EXPLAIN_TEMPLATE_LOADING'] = True
+
+@app.route('/', methods=['GET'])
+def get_users():
+    #print(f"URL is : {flask.url_for('static', filename='css/favicon.ico')}")
+    return flask.render_template('index.html')
+
+@app.route('/<string:page_name>')
+def page(page_name='/'):
+    try:
+        return flask.render_template(page_name)
+    except:
+        return flask.redirect('/')
+
+@app.route('/about',methods=['GET'])
+def about_method():
+	return flask.render_template('about.html')
+
+app.run()    
